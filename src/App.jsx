fac9c6a63f07715866c577ef6b7a1be5f4f617e7
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-07 02:57:52 -0700

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import PriceList from "./components/PriceList/PriceList";
+import Dashboard from "./components/Dashbord/Dashboard";
+import PhoneBar from "./components/PhoneBar/PhoneBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Navbar></Navbar>
@@ -15,6 +13,10 @@ function App() {
       <h1 className="text-7xl text-white bg-purple-500 my-5 font-extrabold p-5 text-center ">Hello! From Tailwind</h1>
 
       <PriceList></PriceList>
+
+      <Dashboard></Dashboard>
+
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
